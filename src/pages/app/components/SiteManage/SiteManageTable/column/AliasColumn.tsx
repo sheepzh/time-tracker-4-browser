a62399d835d3e15b66749ac6158bfbd153f77994
sheepie <returnzhy1996@outlook.com>
2025-10-10 14:33:31 +0800

    feat: retain cateogry filter for site management (#566)

--- src/pages/app/components/SiteManage/SiteManageTable/column/AliasColumn.tsx
+++ src/pages/app/components/SiteManage/SiteManageTable/column/AliasColumn.tsx
@@ -11,10 +11,11 @@ import { MagicStick } from "@element-plus/icons-vue"
 import Flex from "@pages/components/Flex"
 import siteService from "@service/site-service"
 import { getSuffix as getPslSuffix } from "@util/psl"
-import { identifySiteKey } from "@util/site"
-import { ElIcon, ElPopconfirm, ElTableColumn, ElText } from "element-plus"
+import { identifySiteKey, SiteMap } from "@util/site"
+import { ElIcon, ElMessage, ElPopconfirm, ElTableColumn, ElText } from "element-plus"
 import { toUnicode as punyCode2Unicode } from "punycode"
 import { defineComponent, type StyleValue } from "vue"
+import { useSiteManageTable } from '../../useSiteManage'
 
 function cvt2Alias(part: string): string {
     let decoded = part
@@ -38,59 +39,71 @@ export function genInitialAlias(site: timer.site.SiteInfo): string | undefined {
     return parts.reverse().map(cvt2Alias).join(' ')
 }
 
-const _default = defineComponent({
-    emits: {
-        rowAliasSaved: (_row: timer.site.SiteInfo) => true,
-        batchGenerate: () => true,
-    },
-    setup: (_, ctx) => {
-        const handleChange = async (newAlias: string | undefined, row: timer.site.SiteInfo) => {
-            newAlias = newAlias?.trim?.()
-            row.alias = newAlias
-            if (newAlias) {
-                await siteService.saveAlias(row, newAlias)
-            } else {
-                await siteService.removeAlias(row)
-            }
-            ctx.emit("rowAliasSaved", row)
-        }
+const AliasColumn = defineComponent<{}>(() => {
+    const { pagination, refresh } = useSiteManageTable()
 
-        return () => (
-            <ElTableColumn
-                minWidth={160}
-                align="center"
-                v-slots={{
-                    header: () => (
-                        <Flex justify="center" align="center" gap={4}>
-                            <span>{t(msg => msg.siteManage.column.alias)}</span>
-                            <ElPopconfirm
-                                title={t(msg => msg.siteManage.genAliasConfirmMsg)}
-                                width={400}
-                                onConfirm={() => ctx.emit('batchGenerate')}
-                                v-slots={{
-                                    reference: () => (
-                                        <Flex height='100%'>
-                                            <ElText type="primary" style={{ cursor: 'pointer' } satisfies StyleValue}>
-                                                <ElIcon color="primary">
-                                                    <MagicStick />
-                                                </ElIcon>
-                                            </ElText>
-                                        </Flex>
-                                    )
-                                }}
-                            />
-                        </Flex>
-                    ),
-                    default: ({ row }: { row: timer.site.SiteInfo }) => <Editable
-                        key={`${identifySiteKey(row)}_${row.alias}`}
-                        modelValue={row.alias}
-                        initialValue={genInitialAlias(row)}
-                        onChange={val => handleChange(val, row)}
-                    />
-                }}
-            />
-        )
+    const handleChange = async (newAlias: string | undefined, row: timer.site.SiteInfo) => {
+        newAlias = newAlias?.trim?.()
+        row.alias = newAlias
+        if (newAlias) {
+            await siteService.saveAlias(row, newAlias)
+        } else {
+            await siteService.removeAlias(row)
+        }
+        refresh()
     }
+
+    const handleBatchGenerate = async () => {
+        let data = pagination.value?.list
+        if (!data?.length) {
+            return ElMessage.info("No data")
+        }
+        const toSave = new SiteMap<string>()
+        const items = await siteService.batchSelect(data)
+        items.filter(i => !i.alias).forEach(site => {
+            const newAlias = genInitialAlias(site)
+            newAlias && toSave.put(site, newAlias)
+        })
+        await siteService.batchSaveAliasNoRewrite(toSave)
+        refresh()
+        ElMessage.success(t(msg => msg.operation.successMsg))
+    }
+
+    return () => (
+        <ElTableColumn
+            minWidth={160}
+            align="center"
+            v-slots={{
+                header: () => (
+                    <Flex justify="center" align="center" gap={4}>
+                        <span>{t(msg => msg.siteManage.column.alias)}</span>
+                        <ElPopconfirm
+                            title={t(msg => msg.siteManage.genAliasConfirmMsg)}
+                            width={400}
+                            onConfirm={handleBatchGenerate}
+                            v-slots={{
+                                reference: () => (
+                                    <Flex height='100%'>
+                                        <ElText type="primary" style={{ cursor: 'pointer' } satisfies StyleValue}>
+                                            <ElIcon color="primary">
+                                                <MagicStick />
+                                            </ElIcon>
+                                        </ElText>
+                                    </Flex>
+                                )
+                            }}
+                        />
+                    </Flex>
+                ),
+                default: ({ row }: { row: timer.site.SiteInfo }) => <Editable
+                    key={`${identifySiteKey(row)}_${row.alias}`}
+                    modelValue={row.alias}
+                    initialValue={genInitialAlias(row)}
+                    onChange={val => handleChange(val, row)}
+                />
+            }}
+        />
+    )
 })
 
-export default _default
+export default AliasColumn
--- src/pages/app/components/SiteManage/SiteManageTable/column/OperationColumn.tsx
+++ src/pages/app/components/SiteManage/SiteManageTable/column/OperationColumn.tsx
@@ -10,29 +10,29 @@ import { Delete } from "@element-plus/icons-vue"
 import { type ElTableRowScope } from "@pages/element-ui/table"
 import siteService from "@service/site-service"
 import { ElTableColumn } from "element-plus"
-import type { FunctionalComponent } from "vue"
+import { defineComponent } from "vue"
+import { useSiteManageTable } from '../../useSiteManage'
 
-type Props = { onDelete?: ArgCallback<timer.site.SiteInfo> }
+const OperationColumn = defineComponent<{}>(() => {
+    const { refresh } = useSiteManageTable()
+    const handleConfirm = (key: timer.site.SiteKey) => siteService.remove(key).then(refresh).catch(() => { })
+    return () => (
+        <ElTableColumn
+            width={150}
+            label={t(msg => msg.button.operation)}
+            align="center"
+            v-slots={
+                ({ row }: ElTableRowScope<timer.site.SiteInfo>) => (
+                    <PopupConfirmButton
+                        buttonIcon={Delete}
+                        buttonType="danger"
+                        buttonText={t(msg => msg.button.delete)}
+                        confirmText={t(msg => msg.siteManage.deleteConfirmMsg, { host: row.host })}
+                        onConfirm={() => handleConfirm(row)}
+                    />
+                )}
+        />
+    )
+})
 
-const _default: FunctionalComponent<Props> = props => (
-    <ElTableColumn
-        width={150}
-        label={t(msg => msg.button.operation)}
-        align="center"
-        v-slots={
-            ({ row }: ElTableRowScope<timer.site.SiteInfo>) => (
-                <PopupConfirmButton
-                    buttonIcon={Delete}
-                    buttonType="danger"
-                    buttonText={t(msg => msg.button.delete)}
-                    confirmText={t(msg => msg.siteManage.deleteConfirmMsg, { host: row.host })}
-                    onConfirm={async () => {
-                        await siteService.remove(row)
-                        props.onDelete?.(row)
-                    }}
-                />
-            )}
-    />
-)
-
-export default _default
+export default OperationColumn
