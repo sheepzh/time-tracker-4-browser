3fee05513cc72ef07c2927f2cabdc15efd688cc8
sheepzh <returnzhy1996@outlook.com>
2026-02-11 01:07:54 +0800

    chore: exclude files based on .gitignore

--- script/zip.sh
+++ script/zip.sh
@@ -6,12 +6,20 @@ FOLDER=$(
 )
 TARGET_PATH="${FOLDER}/aaa"
 
-COPYFILE_DISABLE=1 tar -zcvf ${TARGET_PATH} \
-    --exclude=dist*/ \
-    --exclude=.git/ \
-    --exclude=package-lock.json \
-    --exclude=node_modules \
-    --exclude=firefox_dev*/ \
-    --exclude=market_packages \
-    --exclude=aaa \
-    ./
+EXCLUDE_ARGS=""
+
+if [ -f "${FOLDER}/.gitignore" ]; then
+    while IFS= read -r line || [ -n "$line" ]; do
+        [[ -z "$line" || "$line" =~ ^# ]] && continue
+        line=$(echo "$line" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+        [ -z "$line" ] && continue
+
+        pattern="${line#/}"
+        EXCLUDE_ARGS="${EXCLUDE_ARGS} --exclude=${pattern}"
+    done < "${FOLDER}/.gitignore"
+fi
+
+EXCLUDE_ARGS="${EXCLUDE_ARGS} --exclude=.git"
+
+cd "${FOLDER}"
+COPYFILE_DISABLE=1 tar -zcf ${TARGET_PATH} ${EXCLUDE_ARGS} ./
