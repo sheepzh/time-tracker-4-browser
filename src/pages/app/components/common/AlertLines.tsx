01fc7f5120408241d95cae51ec7175ec7aaf138c
linwenqi <1491338835@qq.com>
2026-03-30 17:31:25 +0800

    fix: resolve empty alert render when exactly one total rule exists and matches successfully

--- src/pages/app/components/common/AlertLines.tsx
+++ src/pages/app/components/common/AlertLines.tsx
@@ -13,20 +13,25 @@ export type AlertLinesProps = {
     type?: AlertProps['type']
 }
 
-const AlertLines: FunctionalComponent<AlertLinesProps> = ({ lines, title, type }) => (
-    <ElAlert
-        type={type}
-        title={typeof title === 'string' ? title : t(title)}
-        closable={false}
-        style={STYLE}
-    >
-        {lines?.map(l => <li>{
-            Array.isArray(l)
-                ? tN(l[0], l[1])
-                : (typeof l === 'string' ? l : t(l))
-        }</li>)}
-    </ElAlert>
-)
+const AlertLines: FunctionalComponent<AlertLinesProps> = ({ lines, title, type }) => {
+    return (
+        <ElAlert
+            key={type}
+            type={type}
+            title={typeof title === 'string' ? title : t(title)}
+            closable={false}
+            style={STYLE}
+        >
+            {lines?.map((l, i) => (
+                <li key={i}>
+                    {Array.isArray(l)
+                        ? tN(l[0], l[1])
+                        : (typeof l === 'string' ? l : t(l))}
+                </li>
+            ))}
+        </ElAlert>
+    )
+}
 
 AlertLines.displayName = 'AlertLines'
 
