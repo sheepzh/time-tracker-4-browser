cb2174c635e938367086214635b642dc82a59ff7
sheepzh <returnzhy1996@outlook.com>
2026-04-01 16:34:03 +0800

    test: setup

--- test-e2e/setup.ts
+++ test-e2e/setup.ts
@@ -1,6 +1,2 @@
-['SIGINT', 'SIGTERM', 'SIGHUP', 'SIGQUIT', 'SIGUSR1', 'SIGUSR2'].forEach((sig) => {
-  process.on(sig, () => {
-    console.error(`!!! Received signal: ${sig} at ${new Date().toISOString()}`);
-    console.trace();
-  });
+process.on('SIGTERM', () => {
 });
