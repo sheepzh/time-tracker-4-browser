1fd1620817b3461088666f7a676700de06571332
sheepie <returnzhy1996@outlook.com>
2026-05-06 22:22:07 +0800

    fix: file tracking for FF (#752)

--- src/background/track-server/index.ts
+++ src/background/track-server/index.ts
@@ -1,4 +1,5 @@
 import { IS_ANDROID, IS_FIREFOX } from '@util/constant/environment'
+import { isFileUrl } from '@util/pattern'
 import type MessageDispatcher from "../message-dispatcher"
 import FileTracker from './file-tracker'
 import { initTabGroup } from './group'
@@ -7,7 +8,12 @@ import { handleTrackRunTimeEvent } from './runtime'
 
 export default function initTrackServer(messageDispatcher: MessageDispatcher) {
     messageDispatcher
-        .register('track.time', (ev, { tab, url }) => handleTrackTimeEvent(ev, url, tab))
+        .register('track.time', async (ev, { tab }) => {
+            const url = tab?.url
+            // Not to process event from FF file tab
+            if (IS_FIREFOX && url && isFileUrl(url)) return
+            await handleTrackTimeEvent(ev, tab)
+        })
         .register('track.runTime', (ev, { url }) => handleTrackRunTimeEvent(ev, url))
 
     initTabGroup()
