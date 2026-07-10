7f9ac27d4f45002e0a1fefb6f3451ecc452933c3
sheepzh <returnzhy1996@outlook.com>
2026-07-10 20:40:57 +0800

    feat: optimized the name of group

--- src/pages/app/components/Record/Table/columns/GroupColumn.tsx
+++ src/pages/app/components/Record/Table/columns/GroupColumn.tsx
@@ -1,11 +1,32 @@
 import { t } from '@app/locale'
 import { useTabGroups } from "@hooks"
+import Flex from '@pages/components/Flex'
 import { cvtGroupColor } from '@pages/util/style'
 import { isGroup } from "@util/stat"
-import { ElTableColumn, ElTag, type RenderRowData } from "element-plus"
-import { defineComponent, StyleValue } from "vue"
+import { ElTableColumn, type RenderRowData } from "element-plus"
+import { type CSSProperties, defineComponent, type FunctionalComponent } from "vue"
 
-const GroupColumn = defineComponent(() => {
+type CellProps = {
+    row: tt4b.stat.Row
+    map: Record<number, chrome.tabGroups.TabGroup>
+}
+const DOT_STYLE: CSSProperties = {
+    display: 'inline-block',
+    height: '18px',
+    width: '18px',
+    borderRadius: '4px',
+}
+const GroupCell: FunctionalComponent<CellProps> = ({ row, map }) => {
+    if (!isGroup(row)) return null
+    const { groupKey } = row
+    const { color, title } = map[groupKey] ?? {}
+    return <Flex inline justify="center" align="center" gap={4}>
+        <span style={{ backgroundColor: cvtGroupColor(color), ...DOT_STYLE }} />
+        {title ?? `ID: ${groupKey}`}
+    </Flex>
+}
+
+const GroupColumn = defineComponent<{}>(() => {
     const { groupMap } = useTabGroups()
 
     return () => (
@@ -13,21 +34,7 @@ const GroupColumn = defineComponent(() => {
             align="center"
             label={t(msg => msg.item.group)}
             width={140}
-            v-slots={({ row }: RenderRowData<tt4b.stat.Row>) => {
-                if (!isGroup(row)) return
-                const { groupKey } = row
-                const { color, title } = groupMap.value[groupKey] ?? {}
-                return (
-                    <ElTag
-                        style={{
-                            backgroundColor: cvtGroupColor(color),
-                            color: 'var(--el-text-primary-color)',
-                        } satisfies StyleValue}
-                    >
-                        {title ?? `ID: ${groupKey}`}
-                    </ElTag>
-                )
-            }}
+            v-slots={({ row }: RenderRowData<tt4b.stat.Row>) => <GroupCell row={row} map={groupMap.value} />}
         />
     )
 })
