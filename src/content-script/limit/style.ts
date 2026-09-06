7440fd5c7a75b68c703d0a8536e3d19871cca352
sheepzh <returnzhy1996@outlook.com>
2026-09-06 16:03:54 +0800

    chore: CSSStyleProperties => wqCSSStyleDeclaration

--- src/content-script/limit/style.ts
+++ src/content-script/limit/style.ts
@@ -1,5 +1,5 @@
 const camelToKebab = (str: string): string => str.replace(/[A-Z]/g, ch => `-${ch.toLowerCase()}`)
 
-export function mountStyle(el: HTMLElement, style: Partial<CSSStyleProperties>) {
+export function mountStyle(el: HTMLElement, style: Partial<CSSStyleDeclaration>) {
     Object.entries(style).forEach(([key, val]) => el.style.setProperty(camelToKebab(key), String(val ?? '')))
 }
