0679ef753a0bdcf20f57a583badd4a6bad6df17e
sheepzh <returnzhy1996@outlook.com>
2026-09-18 21:36:54 +0800

    feat: semi

--- src/pages/app/components/common/RemoteIcon.tsx
+++ src/pages/app/components/common/RemoteIcon.tsx
@@ -1,7 +1,7 @@
-import { UserFilled } from '@element-plus/icons-vue'
+import { UploadFilled } from '@element-plus/icons-vue'
 import { ElIcon } from 'element-plus'
 import type { FunctionalComponent } from 'vue'
 
 export const RemoteIcon: FunctionalComponent<{ visible?: boolean }> =
-    ({ visible }) => !!visible && <ElIcon><UserFilled /></ElIcon>
+    ({ visible }) => !!visible && <ElIcon><UploadFilled /></ElIcon>
 
